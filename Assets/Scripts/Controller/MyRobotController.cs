6292b4bbca7d263e08a0114601a9e1116e78edb9
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-10-31 11:59:33 +0100

    Modified MyRobotController to allow for single joint control

--- Assets/Scripts/Controller/MyRobotController.cs
+++ Assets/Scripts/Controller/MyRobotController.cs
@@ -3,16 +3,37 @@ using Math;
 
 public class MyRobotController : UnityEngine.MonoBehaviour
 {
-    [UnityEngine.SerializeField] private List<UnityEngine.GameObject> myJoints;
+    [UnityEngine.SerializeField] private List<UnityEngine.GameObject> joints;
+    [UnityEngine.SerializeField] private int activeJoint = 0;
 
-    void Update()
+    private IJoint<Matrix4x4> transJoint;
+    private IJoint<Quaternion> rotJoint;
+
+    private void Start()
     {
-        for (int i = 0; i < myJoints.Count; i++)
-        {
-            if (myJoints[i].GetComponent<IJoint<Math.Matrix4x4>>() != null)
-                myJoints[i].GetComponent<IJoint<Math.Matrix4x4>>().JointUpdate();
-            if (myJoints[i].GetComponent<IJoint<Math.Quaternion>>() != null)
-                myJoints[i].GetComponent<IJoint<Math.Quaternion>>().JointUpdate();
-        }
+        UnityEngine.InputSystem.InputSystem.actions.FindAction("Joint Change").performed += ctx => ChangeJoint();
+        ChangeJoint();
+    }
+
+    private void Update()
+    {
+        if (transJoint != null && UnityEngine.Input.GetKey(transJoint.PositiveMotionKeyId)) transJoint.PositiveMotionAction();
+        if(transJoint != null && UnityEngine.Input.GetKey(transJoint.NegativeMotionKeyId)) transJoint.NegativeMotionAction();
+
+        if(rotJoint != null && UnityEngine.Input.GetKey(rotJoint.PositiveMotionKeyId)) rotJoint.PositiveMotionAction();
+        if(rotJoint != null && UnityEngine.Input.GetKey(rotJoint.NegativeMotionKeyId)) rotJoint.NegativeMotionAction();
+    }
+
+    private void ChangeJoint()
+    {
+        activeJoint++;
+        activeJoint %= joints.Count;
+
+        transJoint = null; rotJoint = null;
+
+        if (joints[activeJoint].TryGetComponent(out transJoint)) return;
+        if (joints[activeJoint].TryGetComponent(out rotJoint)) return;
+
+        throw new System.Exception("Invalid joint");
     }
 }
