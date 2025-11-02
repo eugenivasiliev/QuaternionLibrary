c0debcb95d3443ce5a0ff52b6581e6dc626c216b
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-11-02 22:27:12 +0100

    Parameter adjustment for gamefeel and build

--- Assets/Scripts/Car/Car.cs
+++ Assets/Scripts/Car/Car.cs
@@ -7,16 +7,20 @@ public class Car : UnityEngine.MonoBehaviour
     [UnityEngine.SerializeField] private Vector3 cameraOffset = 10d * Vector3.up + 10d * Vector3.back;
     CyclicDouble rotationAngle = CyclicDouble.cyclicAngleDouble;
 
+    [UnityEngine.Header("Speed")]
+    [UnityEngine.SerializeField] private double speed = 0.1d;
+    [UnityEngine.SerializeField] private double rotationSpeed = 0.01d;
+
     [UnityEngine.Header("Obstacles")]
     [UnityEngine.SerializeField] private string obstacleTag;
 
     private void Update()
     {
         UnityEngine.Camera.main.transform.position = this.Transform.position + this.Transform.rotation * cameraOffset;
-        UnityEngine.Camera.main.transform.LookAt(this.transform);
+        UnityEngine.Camera.main.transform.rotation = this.Transform.rotation * new Quaternion(Vector3.right, 45d, true);
 
-        this.Transform.position += this.Transform.rotation * (UnityEngine.Input.GetAxis("Vertical") * Vector3.forward);
-        rotationAngle -= UnityEngine.Input.GetAxis("Horizontal") * 0.1d;
+        this.Transform.position += this.Transform.rotation * (UnityEngine.Input.GetAxis("Vertical") * speed * Vector3.forward);
+        rotationAngle -= UnityEngine.Input.GetAxis("Horizontal") * rotationSpeed;
         this.Transform.rotation = new Quaternion(Vector3.up, rotationAngle);
     }
 
